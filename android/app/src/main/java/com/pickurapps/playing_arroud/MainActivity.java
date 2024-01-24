6fc680f505dcb4add27c5f03cb84ee06c4814f66
Ala ben yahia <alabinyahia@gmail.com>
2024-01-24 09:51:58 +0100

    Finish Register form

---
+++ android/app/src/main/java/com/pickurapps/playing_arroud/MainActivity.java
@@ -0,0 +1,6 @@
+package com.pickurapps.playing_arroud;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/register.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+class Register extends StatelessWidget {
+  const Register({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Register'),
+      ),
+      body: const RegisterForm(),
+    );
+  }
+}
+
+
+class RegisterForm extends StatefulWidget {
+  const RegisterForm({super.key});
+
+  @override
+  State<RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  final formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.person),
+                    hintText: 'Enter you name',
+                    labelText: 'Name'
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.contact_emergency),
+                    hintText: 'Enter you gym name',
+                    labelText: 'Gym name'
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.email),
+                    hintText: 'Enter you email address',
+                    labelText: 'Email address'
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.password),
+                  hintText: 'Enter you password',
+                  labelText: 'Password',
+
+                ),
+                obscureText: true,
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
